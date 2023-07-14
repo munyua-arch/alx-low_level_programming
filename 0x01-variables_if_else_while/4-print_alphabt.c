dc2571907878fb188af5cd2ddaad7ebff0744cad
Dennis <dmurimi919@gmail.com>
2023-07-14 05:23:23 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
