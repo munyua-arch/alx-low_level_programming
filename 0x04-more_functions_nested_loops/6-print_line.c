728db9a58ddb28d1a44ad818f1399ba6ffd9f0d7
Dennis <dmurimi919@gmail.com>
2023-07-20 05:03:56 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints an underscore n times
+ * @n: number of times printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
