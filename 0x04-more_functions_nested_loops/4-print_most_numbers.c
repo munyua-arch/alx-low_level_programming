2ccbb0a48402cd7e7422c5524135446458f54ab9
Dennis <dmurimi919@gmail.com>
2023-07-20 02:53:52 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
-	for (i = '48'; i < '58'; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != '50' && i != '52')
+		if (i != 50 && i != 52)
 		{
 			_putchar(i);
 		}
