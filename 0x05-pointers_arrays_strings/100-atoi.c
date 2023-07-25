353db01e88230242ae8aa7a7d6036373d63ef7ab
Dennis <dmurimi919@gmail.com>
2023-07-25 05:11:13 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,6 +9,7 @@
 int _atoi(char *s)
 {
 	int a, b, c, d, e, num;
+
 	a = 0;
 	b = 0;
 	c = 0;
@@ -18,15 +19,17 @@ int _atoi(char *s)
 
 	while (s[d] != '\0')
 		d++;
+
 	while (a < d && e == 0)
 	{
 		if (s[a] == '-')
 			++b;
+
 		if (s[a] >= '0' && s[a] <= '9')
 		{
 			num = s[a] - '0';
 			if (b % 2)
-				num = num - 1;
+				num = -num;
 			c = c * 10 + num;
 			e = 1;
 			if (s[a + 1] < '0' || s[a + 1] > '9')
@@ -35,7 +38,10 @@ int _atoi(char *s)
 		}
 		a++;
 	}
+
 	if (e == 0)
 		return (0);
+
 	return (c);
 }
+
