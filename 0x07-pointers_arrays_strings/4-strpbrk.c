cc42947c9eb0011863f47cb50aaef21ab9206d15
Dennis <dmurimi919@gmail.com>
2023-07-31 10:15:29 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - matches any character specified
+ * @s: This is the C string to be scanned.
+ * @accept:character in str1 that matches one of the characters in str2
+ * Return: string s that matches any character specified in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			{
+				return (a);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
