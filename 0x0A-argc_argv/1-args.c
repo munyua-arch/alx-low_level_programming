35449c9c745c237d23214c86bfbd0d34f4f435da
Dennis <dmurimi919@gmail.com>
2023-08-04 11:23:22 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,19 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int a;
-
-	if (argc == 1)
-	{
-		printf("%d\n", argc - 1);
-	}
-	else
-	{
-		for (a = 0; *argv; a++, argv++)
-		{
-			printf("%d\n", a - 1);
-		}
-	}
-
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
