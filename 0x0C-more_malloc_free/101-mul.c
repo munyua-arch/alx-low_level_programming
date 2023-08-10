bd0a19bb450e4d030ac8096338b9e567574de863
Dennis <dmurimi919@gmail.com>
2023-08-10 09:07:43 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,55 +1,92 @@
-#include "main.h"
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isnumber - checks if string is number
+ *
+ * @s: string
+ *
+ * Return: 1 if number, 0 if not
+ */
+int _isnumber(char *s)
+{
+	int i, check, d;
+
+	d = 0, check = 1;
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
+}
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
+ * _callocX - reserves memory initialized to 0
  *
- * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ * @nmemb: # of bytes
+ *
+ * Return: pointer
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+char *_callocX(unsigned int nmemb)
 {
-	void *pointer;
-	char *copy, *f;
-	unsigned int a;
+	unsigned int i;
+	char *p;
 
-	if (new_size == old_size)
-		return (ptr);
+	p = malloc(nmemb + 1);
+	if (p == 0)
+		return (0);
+	for (i = 0; i < nmemb; i++)
+		p[i] = '0';
+	p[i] = '\0';
+	return (p);
+}
 
-	if (ptr == NULL)
+/**
+ * main - program that multiplies two positive numbers.
+ *
+ * @argc: count
+ * @argv: vector
+ * Return: output
+ */
+int main(int argc, char **argv)
+{
+	int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
+	char *res;
+
+	if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0)
+		printf("Error\n"), exit(98);
+	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+		printf("0\n"), exit(0);
+	l1 = strlen(argv[1]), l2 = strlen(argv[2]);
+	lful = l1 + l2;
+	res = _callocX(lful);
+	if (res == 0)
+		printf("Error\n"), exit(98);
+	for (i = l2 - 1; i >= 0; i--)
 	{
-		pointer = malloc(new_size);
-
-		if (pointer == NULL)
-			return (NULL);
-
-		return (pointer);
+		ten = 0, ten2 = 0;
+		for (j = l1 - 1; j >= 0; j--)
+		{
+			tl = i + j + 1;
+			mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten;
+			ten =  mul / 10;
+			add = (res[tl] - '0') + (mul % 10) + ten2;
+			ten2 = add / 10;
+			res[tl] = (add % 10) + '0';
+		}
+		res[tl - 1] = (ten + ten2) + '0';
 	}
-
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	copy = ptr;
-	pointer = malloc(sizeof(*copy) * new_size);
-
-	if (pointer == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	f = pointer;
-
-	for (a = 0; a < old_size && a < new_size; a++)
-		f[a] = *copy++;
-
-	free(ptr);
-	return (pointer);
+	if (res[0] == '0')
+		zer = 1;
+	for (; zer < lful; zer++)
+		printf("%c", res[zer]);
+	printf("\n");
+	free(res);
+	return (0);
 }
