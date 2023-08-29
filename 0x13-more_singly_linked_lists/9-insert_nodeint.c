f6bd5cf0ae5d34b473513acbcf1dafb5186a08ca
munyua-arch <dmurimi919@gmail.com>
2023-08-29 08:03:57 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,32 +14,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *add, *cp = *head;
+	listint_t *new, *copy = *head;
 	unsigned int node;
 
-	add = malloc(sizeof(listint_t));
-	if (add == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
-	add->n = n;
+	new->n = n;
 
 	if (idx == 0)
 	{
-		add->next = copy;
-		*head = add;
-		return (add);
+		new->next = copy;
+		*head = new;
+		return (new);
 	}
 
 	for (node = 0; node < (idx - 1); node++)
 	{
-		if (cp == NULL || cp->next == NULL)
+		if (copy == NULL || copy->next == NULL)
 			return (NULL);
 
-		cp = cp->next;
+		copy = copy->next;
 	}
 
-	add->next = cp->next;
-	cp->next = new;
+	new->next = copy->next;
+	copy->next = new;
 
-	return (add);
+	return (new);
 }
