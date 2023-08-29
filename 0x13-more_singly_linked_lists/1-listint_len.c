86edd67d8e2b4165a7855c797daa32b9e1d53ac6
munyua-arch <dmurimi919@gmail.com>
2023-08-29 00:29:13 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,24 @@
 #include "lists.h"
 #include <stdio.h>
 
+
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list_t list.
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
  *
- * Return: The number of nodes in the list_t list.
+ * Return: The number of elements in the listint_t list.
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num_nodes = 0;
 
 	while (h)
 	{
-		nodes++;
+		num_nodes++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num_nodes);
 }
