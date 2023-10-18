8960e2ad7702bf7b296960825e3edd9b3caaf8aa
munyua-arch <dmurimi919@gmail.com>
2023-10-18 01:09:02 -0700

    almighty dynamic push

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String
+ * @c: Character to search
+ * Return: Pointer to the first occurrence of the character c in the string s
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - prints the consecutive caracters of s1 that are in s2.
+ * @s: source string
+ * @accept: searching string
+ *
+ * Return: new string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b;
+
+	for (b = 0; *(s + b); b++)
+	{
+		for (a = 0; *(accept + a); a++)
+		{
+			if (*(s + b) == *(accept + a))
+				break;
+		}
+	if (*(accept + a) == '\0')
+		break;
+	}
+	return (b);
+}
